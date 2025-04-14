d18bb3695b62037ab8ec5832a8a9fe14243d72d8
gabriel-g4 <gbgarcia642@gmail.com>
2025-04-14 20:53:09 -0300

    feat: clase 14 delegados, func, action

--- Fundamentos Csharp/Fundamentos Csharp/Clase13.cs
+++ Fundamentos Csharp/Fundamentos Csharp/Clase13.cs
@@ -11,7 +11,7 @@ namespace Fundamentos_Csharp
 {
     internal class Clase13
     {
-        static void Main(string[] args)
+        public static void Clase()
         {
             Console.WriteLine("Clase 13");
             Console.WriteLine("Excepciones\n");
---
+++ Fundamentos Csharp/Fundamentos Csharp/Clase14.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos_Csharp
+{
+    internal class Clase14
+    {
+        // delegate int Mostrar(string mensaje);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Clase 14");
+            Console.WriteLine("Delegados, Func, Action\n");
+
+
+            // Con delegado:
+            // Mostrar mostrar = Show;
+
+            // Con Func
+            //Func<string, int> mostrar = Show;
+
+            // Action es como Func, pero sin retorno
+            // Action<string, string> mostrar = Show;
+
+            // Ejemplo con función anónima
+            Action<string, string> mostrar = (a, b) => { Console.WriteLine(a + b); };
+
+
+            HacerAlgo(mostrar);
+
+
+
+        }
+
+        static void HacerAlgo(Action<string, string> funcion)
+        {
+            Console.WriteLine("Hacer Algo");
+            funcion("Se envio desde otra funcion", " parametro 2");
+            Console.WriteLine("Hacer otra cosa");
+        }
+
+        //public static int Show(string mensaje)
+        //{
+        //    return mensaje.Length;
+        //}
+
+        //public static void Show(string mensaje, string mensaje2)
+        //{
+        //    Console.WriteLine(mensaje + mensaje2);
+        //}
+    }
+}
